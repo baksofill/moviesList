46030b8788a156fea7da9b16b642f1ac520bba4f
Anton Obertynskyi <aobertynskyi@exadel.com>
2018-06-26 16:10:51 +0300

    adding new fields to form

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -28,61 +28,6 @@ var initialData = [
             {hr: "0.5"}
         ]
     }
-    // {
-    //     author: {
-    //         label: "author",
-    //         type: "input",
-    //         class: "myClass, someClass",
-    //         value: "Spilberg",
-    //         validationRule: "string"
-    //     },
-    //     movieName: {
-    //         label: "Director",
-    //         type: "input",
-    //         class: "",
-    //         value: "Cool movie about Marionette",
-    //         validationRule: "string"
-    //     },
-    //     dropdown: [
-    //         {
-    //             option: "Movie",
-    //             class: "",
-    //             disabled: "true",
-    //             type: "dropdown"
-    //         },
-    //         {
-    //             option: "TV series",
-    //             class: "",
-    //             disabled: "false",
-    //             type: "dropdown"
-    //         }
-    //     ],
-    //     releaseDate: {
-    //         label: "Release Date",
-    //         type: "input",
-    //         value: "2007",
-    //         class: "",
-    //         disabled: "true",
-    //         validationRule: "integer"
-    //     },
-    //     duration: [
-    //         {
-    //             min: "30",
-    //             type: "input",
-    //             label: "Min",
-    //             validationRule: "integer",
-    //             class: ""
-    //         },
-    //         {
-    //             hr: "0.5",
-    //             type: "input",
-    //             label: "Min",
-    //             validationRule: "positiveNumber",
-    //             class: ""
-    //         }
-    //     ]
-    // }
-
 ];
 
 var app = new Marionette.Application({
